eccc1c383cda0b9d0c7311cb48233a1de04f1de6
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2012-01-07 04:33:19 +0000

    初始导入。

---
+++ trunk/evolve/evolve-web/src/main/java/com/tfc/evolve/service/exception/BusinessException.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2011 Focus Technology, Co., Ltd. All rights reserved.
+ */
+package com.tfc.evolve.service.exception;
+
+/**
+ * BusinessException.java
+ * 
+ * @author taofucheng
+ */
+public class BusinessException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 4689404986581989383L;
+
+}
