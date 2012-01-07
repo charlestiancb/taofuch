e045a5e2a330b3e0ffdd4c24cb764d8083870d7d
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2012-01-07 04:33:19 +0000

    初始导入。

---
+++ evolve/evolve-web/src/main/java/com/tfc/evolve/model/AlternateResult.java
@@ -0,0 +1,43 @@
+package com.tfc.evolve.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * <li>与“进化者”交互的结果</li>
+ * 
+ * @author taofucheng
+ * 
+ */
+public class AlternateResult implements Serializable {
+    private static final long serialVersionUID = -5311506842103768643L;
+    /** 聊天历史记录 */
+    private List<String> history;
+    /** “进化者”讲的话 */
+    private String reply;
+
+    public void addHistory(String historyContent) {
+        if (history == null) {
+            history = new ArrayList<String>();
+        }
+        history.add(historyContent);
+    }
+
+    public List<String> getHistory() {
+        return history;
+    }
+
+    public void setHistory(List<String> history) {
+        this.history = history;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+}
---
+++ evolve/evolve-web/src/main/java/com/tfc/evolve/service/EvolveService.java
@@ -0,0 +1,33 @@
+package com.tfc.evolve.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tfc.evolve.model.AlternateResult;
+
+@Service
+@Transactional(readOnly = true)
+public class EvolveService {
+    /**
+     * 这是交互入口处。所有的与“进化者”聊天的入口。
+     * 
+     * @param content
+     * @return
+     */
+    public AlternateResult alternate(String content) {
+        AlternateResult result = new AlternateResult();
+        // 获取对应的历史信息
+        result.addHistory(welcome());
+        return result;
+    }
+
+    /**
+     * 欢迎语
+     * 
+     * @return
+     */
+    public String welcome() {
+        // TODO 需要根据不同的角色和用户来给出相应的提示语
+        return "evolution：你好啊，陌生人。我有什么可以帮助你的吗？";
+    }
+}
