31b0d0a555ac1790e20e4275f21258a3336b2318
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2012-06-10 14:25:20 +0000

    增加Htmlparser功能

---
+++ trunk/e-business/business-crawler/src/main/java/com/cloudtech/ebusi/crawler/parser/AliParser.java
@@ -0,0 +1,20 @@
+package com.cloudtech.ebusi.crawler.parser;
+
+import net.vidageek.crawler.Page;
+import net.vidageek.crawler.Url;
+
+public class AliParser extends Parser {
+
+	@Override
+	public boolean followUrl(Url url) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void doParse(Page page) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- trunk/e-business/business-crawler/src/main/java/com/cloudtech/ebusi/crawler/parser/Parser.java
+++ trunk/e-business/business-crawler/src/main/java/com/cloudtech/ebusi/crawler/parser/Parser.java
@@ -1,5 +1,9 @@
 package com.cloudtech.ebusi.crawler.parser;
 
+import net.vidageek.crawler.Page;
+import net.vidageek.crawler.Status;
+import net.vidageek.crawler.Url;
+
 /**
  * 对Html页面进行解析读取操作的一些解析功能。
  * 
@@ -8,9 +12,28 @@ package com.cloudtech.ebusi.crawler.parser;
  */
 public abstract class Parser {
 	/**
-	 * 解析页面内容
+	 * 判断这个URL链接是否是应该进行操作的内容。
 	 * 
-	 * @param pageContent
+	 * @param url
+	 *            当前预操作的url链接。
+	 * @return
 	 */
-	public abstract void doParse(String pageContent);
+	public abstract boolean followUrl(final Url url);
+
+	/**
+	 * 当读取页面时发生错误而无法正常读取时，进行相应的处理。
+	 * 
+	 * @param url
+	 * @param statusError
+	 */
+	public void parseError(Url url, Status statusError) {
+		// TODO 错误时的处理方式，记录已经瓟取过的页面！;
+	}
+
+	/**
+	 * 对读取的页面进行处理。比如：是一个列表页面。
+	 * 
+	 * @param page
+	 */
+	public abstract void doParse(Page page);
 }
