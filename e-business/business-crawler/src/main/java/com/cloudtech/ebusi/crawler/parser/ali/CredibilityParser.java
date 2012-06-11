e457350bba392a4c84d0deb961904d08805b42bd
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2012-06-11 15:15:45 +0000

    增加Htmlparser功能

---
+++ e-business/business-crawler/src/main/java/com/cloudtech/ebusi/crawler/parser/ali/CredibilityParser.java
@@ -0,0 +1,22 @@
+package com.cloudtech.ebusi.crawler.parser.ali;
+
+/**
+ * 阿里会员的诚信档案解析器
+ * 
+ * @author taofucheng
+ * 
+ */
+public class CredibilityParser {
+	/**
+	 * 判断是否符合信用档案的要求，即：是否开通了诚信通
+	 * 
+	 * @param credibilityLink
+	 *            公司信用档案的页面链接
+	 * @return
+	 */
+	public static boolean accept(String credibilityLink) {
+		// TODO 获取信息档案的信息，判断是否拥有信用档案
+		return false;
+	}
+
+}
---
+++ e-business/business-crawler/src/main/java/com/cloudtech/ebusi/crawler/parser/ali/ProfileParser.java
@@ -0,0 +1,22 @@
+package com.cloudtech.ebusi.crawler.parser.ali;
+
+import org.htmlparser.util.NodeList;
+
+/**
+ * 阿里会员信息解析器
+ * 
+ * @author taofucheng
+ * 
+ */
+public class ProfileParser {
+	/**
+	 * 解析用户信息，并将这些信息进行索引！
+	 * 
+	 * @param nl
+	 */
+	public static void indexComInfo(NodeList nl) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
