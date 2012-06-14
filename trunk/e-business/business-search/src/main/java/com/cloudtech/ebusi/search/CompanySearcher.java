c46cb8c32fd2e0979aad62a83eb23cbae2e2da98
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2012-06-14 14:34:58 +0000

    增加Htmlparser功能

--- trunk/e-business/business-search/src/main/java/com/cloudtech/ebusi/search/CompanySearcher.java
+++ trunk/e-business/business-search/src/main/java/com/cloudtech/ebusi/search/CompanySearcher.java
@@ -1,5 +1,20 @@
 package com.cloudtech.ebusi.search;
 
-public class CompanySearcher {
+import java.io.IOException;
 
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.store.FSDirectory;
+
+import com.cloudtech.ebusi.index.CompanyIndexer;
+
+public class CompanySearcher {
+	private static IndexSearcher is = null;
+	static {
+		try {
+			is = new IndexSearcher(IndexReader.open(FSDirectory.open(CompanyIndexer.indexDir)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
