cf9c49d09d4a6943bbb58d9df4a32a0c90576bbb
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2012-06-27 15:27:44 +0000

    增加抓取慧聪的信息

---
+++ e-business/business-crawler/src/main/java/com/cloudtech/ebusi/crawler/parser/hc/CredibilityParser.java
@@ -0,0 +1,37 @@
+package com.cloudtech.ebusi.crawler.parser.hc;
+
+import org.apache.commons.lang.StringUtils;
+import org.htmlparser.util.NodeList;
+
+import com.cloudtech.ebusi.crawler.parser.CompanyInfo;
+
+/**
+ * 慧聪会员的诚信档案解析器
+ * 
+ * @author taofucheng
+ * 
+ */
+public class CredibilityParser {
+	/**
+	 * 判断是否符合信用档案的要求，即：是否开通了诚信通
+	 * 
+	 * @param credibilityLink
+	 *            公司信用档案的页面链接
+	 * @return
+	 */
+	public static boolean accept(NodeList nl) {
+		if (nl == null) {
+			return false;
+		}
+		CompanyInfo com = new CompanyInfo();
+		ProfileParser.parseCred(nl, com);
+		String chengXingTong = com.getDetails().get(CompanyInfo.CRED_NUM);
+		String xinYongbianMa = com.getDetails().get(CompanyInfo.CRED_CODE);
+		if ((StringUtils.isNotBlank(chengXingTong) && Integer.parseInt(chengXingTong) > 0)
+				|| StringUtils.isNotBlank(xinYongbianMa)) {
+			return true;// 如果存在诚信通指数和信用编码，则认为是可以抓取的！
+		}
+		return false;
+	}
+
+}
