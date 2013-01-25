d9a8b13675d1f65e332b7de932238f6d2a3b8ac0
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-25 14:57:11 +0000

    完善一下新浪微博

--- crawler/weibo-crawler/src/main/java/com/scoop/crawler/weibo/request/ExploreRequest.java
+++ crawler/weibo-crawler/src/main/java/com/scoop/crawler/weibo/request/ExploreRequest.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
@@ -82,7 +83,8 @@ public class ExploreRequest {
 			driver.findElement(By.name("loginname")).sendKeys(LogonInfo.getLogonInfo().getUsername());
 			driver.findElement(By.name("password")).sendKeys(LogonInfo.getLogonInfo().getPassword());
 			Thread.sleep(2 * 1000);// 2秒
-			if (driver.findElement(By.name("door")) != null && driver.findElement(By.name("door")).isDisplayed()) {
+			WebElement door = driver.findElement(By.name("door"));
+			if (door != null && door.isDisplayed()) {
 				Thread.sleep(5 * 1000);// 等待5s
 				System.out.println("你也看到了，要输入验证码的，我做不了了！我撤了，拜拜~");
 				driver.quit();
