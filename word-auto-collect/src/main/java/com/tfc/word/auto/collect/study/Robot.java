7f9ee6b5856671affc800741803512e6bc6515c4
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-28 12:52:59 +0000

    增加自动学习新词功能

--- word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/Robot.java
+++ word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/Robot.java
@@ -1,6 +1,11 @@
 package com.tfc.word.auto.collect.study;
 
 public class Robot implements Runnable {
+	private String startUrl;
+
+	public Robot(String startUrl) {
+		this.startUrl = startUrl;
+	}
 
 	public void run() {
 		// TODO Auto-generated method stub
