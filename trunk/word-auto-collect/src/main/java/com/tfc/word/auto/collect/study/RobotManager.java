0f98cb62a320d9905945dd396389a94d242ec7a7
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-28 06:43:49 +0000

    增加自动学习新词功能

---
+++ trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/RobotManager.java
@@ -0,0 +1,19 @@
+package com.tfc.word.auto.collect.study;
+
+/**
+ * 网络爬虫自动学习入口管理。
+ * 
+ * @author taofucheng
+ * 
+ */
+public class RobotManager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
