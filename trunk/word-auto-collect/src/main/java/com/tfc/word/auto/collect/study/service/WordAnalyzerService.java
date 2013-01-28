279b82897ed955c7b37d929a0ed9f1291e3bf353
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-28 12:52:59 +0000

    增加自动学习新词功能

---
+++ trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/service/WordAnalyzerService.java
@@ -0,0 +1,19 @@
+package com.tfc.word.auto.collect.study.service;
+
+/**
+ * 词语分析的服务入口
+ * 
+ * @author taofucheng
+ * 
+ */
+public class WordAnalyzerService {
+	/**
+	 * 对给定的文件进行分析。
+	 * 
+	 * @param text
+	 */
+	public void analyzer(String text) {
+		// 数字、英文、符号，都不作为词
+		// 操作步骤：先对整个内容进行分词。将非符号分隔的所有单字拼接成词
+	}
+}
