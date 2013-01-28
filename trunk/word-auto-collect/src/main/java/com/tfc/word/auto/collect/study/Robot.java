af41aa1499ac31fb5770735eec298d3103ddc4c4
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-28 11:21:15 +0000

    增加自动学习新词功能

---
+++ trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/Robot.java
@@ -0,0 +1,8 @@
+package com.tfc.word.auto.collect.study;
+
+public class Robot implements Runnable {
+
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+}
--- trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/RobotManager.java
+++ trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/RobotManager.java
@@ -1,5 +1,8 @@
 package com.tfc.word.auto.collect.study;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 网络爬虫自动学习入口管理。
  * 
@@ -7,13 +10,24 @@ package com.tfc.word.auto.collect.study;
  * 
  */
 public class RobotManager {
+	private static int THREAD_COUNT = 5;
+	private static ExecutorService threads = Executors.newFixedThreadPool(THREAD_COUNT);
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
+	public static void exec(Robot robot) {
+		threads.execute(robot);
+	}
+
+	/**
+	 * 等待执行完成之后关闭。
+	 */
+	public static void shutdown() {
+		threads.shutdown();
+	}
 }
