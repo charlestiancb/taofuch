18e831ea9e2b2fa86df296f41fd4ed218e6cb8d5
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-28 10:55:52 +0000

    增加自动学习新词功能

--- word-auto-collect/src/main/resources/init/design/tables.sql
+++ word-auto-collect/src/main/resources/init/design/tables.sql
@@ -7,9 +7,9 @@ use words;
 -- ----------------------------
 CREATE TABLE `word_base` (
   `rec_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `word` varchar(500) DEFAULT NULL COMMENT '具体的词语内容',
-  `special_type` varchar(20) DEFAULT NULL COMMENT '专用类型，如学科分类。0：通用类型。',
-  `check_status` varchar(20) DEFAULT NULL COMMENT '审核状态，是否已经被审核通过。0：未审核；1：审核通过',
-  `stat_num` bigint(20) DEFAULT NULL COMMENT '自动学习时，统计得到的数字',
+  `word` varchar(500) NOT NULL COMMENT '具体的词语内容',
+  `special_type` varchar(20) DEFAULT '0' COMMENT '专用类型，如学科分类。0：通用类型。',
+  `check_status` varchar(20) DEFAULT '0' COMMENT '审核状态，是否已经被审核通过。0：未审核；1：审核通过',
+  `stat_num` bigint(20) DEFAULT 0 COMMENT '自动学习时，统计得到的数字',
   PRIMARY KEY (`rec_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '词语的基本信息';
