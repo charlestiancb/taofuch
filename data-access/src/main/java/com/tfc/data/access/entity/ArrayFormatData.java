ffa8cf7dfb3d05f7575dc70f23600098cee19d06
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-29 12:19:41 +0000

    增加数据读写组件

--- data-access/src/main/java/com/tfc/data/access/entity/ArrayFormatData.java
+++ data-access/src/main/java/com/tfc/data/access/entity/ArrayFormatData.java
@@ -1,19 +1,68 @@
 package com.tfc.data.access.entity;
 
+import com.alibaba.fastjson.JSON;
+import com.tfc.data.access.LuceneDataAccess;
+
 /**
  * 数组形式的格式。
  * 
  * @author taofucheng
  * 
  */
-public class ArrayFormatData {
+public class ArrayFormatData extends AbstractFormatData {
 	private static final String prefix = "array";
 	private String instanceName;
-	private long len;
+	private int len;
 
-	public ArrayFormatData(String instanceName, long len) {
-		this.instanceName = instanceName;
+	/**
+	 * 类似这样的定义：new int[2]
+	 * 
+	 * @param instanceName
+	 * @param len
+	 */
+	public ArrayFormatData(String instanceName, int len) {
+		this.instanceName = instanceName + System.nanoTime();
 		this.len = len;
 	}
 
+	/**
+	 * 类似这样的定义：new int[]{1,2}
+	 * 
+	 * @param instanceName
+	 * @param elements
+	 */
+	public ArrayFormatData(String instanceName, Object... elements) {
+		this.instanceName = instanceName;
+		this.len = elements == null ? 0 : elements.length;
+		if (this.len > 0) {
+			for (int i = 0; i < this.len; i++) {
+				save(i, elements[i]);
+			}
+		}
+	}
+
+	public void save(int index, Object object) {
+		LuceneDataAccess.save(genarateKey(index), JSON.toJSONString(object));
+	}
+
+	public Object get(int index, Class<?> targetElementClass) {
+		String value = LuceneDataAccess.findValueByKey(genarateKey(index));
+		return parseToObject(targetElementClass, value);
+	}
+
+	public int getInt(int index) {
+		return (Integer) get(index, Integer.class);
+	}
+
+	public String getString(int index) {
+		return (String) get(index, String.class);
+	}
+
+	public float getFloat(int index) {
+		return (Float) get(index, Float.class);
+	}
+
+	private String genarateKey(int index) {
+		return prefix + "_" + instanceName + "_" + index;
+	}
 }
--- data-access/src/main/java/com/tfc/data/access/entity/KeyValueFormatData.java
+++ data-access/src/main/java/com/tfc/data/access/entity/KeyValueFormatData.java
@@ -1,5 +1,44 @@
 package com.tfc.data.access.entity;
 
-public class KeyValueFormatData {
+import com.alibaba.fastjson.JSON;
+import com.tfc.data.access.LuceneDataAccess;
 
+/**
+ * 键值对结构的格式。如Map类型。
+ * 
+ * @author taofucheng
+ * 
+ */
+public class KeyValueFormatData extends AbstractFormatData {
+	private static final String prefix = "map";
+	private String instanceName = "";
+
+	public KeyValueFormatData(String instanceName) {
+		this.instanceName = instanceName + System.nanoTime();
+	}
+
+	public void put(Object key, Object value) {
+		LuceneDataAccess.save(genarateKey(key), JSON.toJSONString(value));
+	}
+
+	public String getString(Object key) {
+		return (String) get(key, String.class);
+	}
+
+	public int getInt(Object key) {
+		return (Integer) get(key, Integer.class);
+	}
+
+	public float getFloat(Object key) {
+		return (Float) get(key, Float.class);
+	}
+
+	public Object get(Object key, Class<?> targetElementClass) {
+		String value = LuceneDataAccess.findValueByKey(genarateKey(key));
+		return parseToObject(targetElementClass, value);
+	}
+
+	private String genarateKey(Object key) {
+		return prefix + "_" + instanceName + "_" + JSON.toJSONString(key);
+	}
 }
