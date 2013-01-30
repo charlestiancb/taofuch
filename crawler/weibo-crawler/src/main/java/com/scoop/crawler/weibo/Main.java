0c11f0af6ce1dd93603bc9a5590fa9c85d17bcd6
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-01-30 11:36:09 +0000

    初始导入。

--- crawler/weibo-crawler/src/main/java/com/scoop/crawler/weibo/Main.java
+++ crawler/weibo-crawler/src/main/java/com/scoop/crawler/weibo/Main.java
@@ -20,9 +20,9 @@ public class Main {
 		// 常见的个人微博页面
 		// String url = "http://weibo.com/yaoyaoxiaojing";//
 		// 加V的个人微博官网形式
-		// String url = "http://gov.weibo.com/profile.php?uid=sciencenet&ref=";
+		String url = "http://gov.weibo.com/profile.php?uid=sciencenet&ref=";
 		// 微博搜索页面，以下示例中{0}就是words中每一行的内容
-		String url = "http://s.weibo.com/weibo/{0}&Refer=STopic_box";
+		// String url = "http://s.weibo.com/weibo/{0}&Refer=STopic_box";
 		// 微博搜索用户
 		// url =
 		// "http://s.weibo.com/user/%25E7%25A4%25BE%25E4%25BC%259A%25E8%25AE%25A1%25E7%25AE%2597";
