43dacea1e1c629329457194e910564b70dc96f85
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-04 13:29:30 +0000

    完善一下新浪微博

--- weibo-1.1/src/main/java/com/scoop/crawler/weibo/runnable/WeiboCommentRunnable.java
+++ weibo-1.1/src/main/java/com/scoop/crawler/weibo/runnable/WeiboCommentRunnable.java
@@ -1,8 +1,12 @@
 package com.scoop.crawler.weibo.runnable;
 
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import com.scoop.crawler.weibo.parser.CommentParser;
 import com.scoop.crawler.weibo.repository.DataSource;
+import com.scoop.crawler.weibo.repository.mysql.Weibo;
 import com.scoop.crawler.weibo.request.failed.FailedHandler;
+import com.scoop.crawler.weibo.util.ThreadUtils;
 
 public class WeiboCommentRunnable extends Thread implements Runnable {
 	protected DataSource dataSource;
@@ -14,6 +18,13 @@ public class WeiboCommentRunnable extends Thread implements Runnable {
 	}
 
 	public void run() {
-		new CommentParser(dataSource, handler).fetchWeiboComments();
+		DefaultHttpClient client = ThreadUtils.allocateHttpClient();
+		CommentParser cp = new CommentParser(dataSource, handler);
+		for (Weibo w = dataSource.getOneUnfetchedWeibo(); w != null; w = dataSource.getOneUnfetchedWeibo()) {
+			cp.fetchWeiboComments(w, client);
+		}
+		// 将线程释放
+		ThreadUtils.freeThread();
+		ThreadUtils.finishComment();
 	}
 }
