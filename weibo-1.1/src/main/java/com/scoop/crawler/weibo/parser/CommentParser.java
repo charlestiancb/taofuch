4fff949fc053d4562c33d4008848f07968b1a7ee
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-04 09:33:15 +0000

    将后台运行改为浏览器运行。

---
+++ weibo-1.1/src/main/java/com/scoop/crawler/weibo/parser/CommentParser.java
@@ -0,0 +1,11 @@
+package com.scoop.crawler.weibo.parser;
+
+/**
+ * 微博评论解析器。
+ * 
+ * @author taofucheng
+ * 
+ */
+public class CommentParser {
+
+}
---
+++ weibo-1.1/src/main/java/com/scoop/crawler/weibo/parser/FansParser.java
@@ -0,0 +1,11 @@
+package com.scoop.crawler.weibo.parser;
+
+/**
+ * 用户粉丝信息解析器。
+ * 
+ * @author taofucheng
+ * 
+ */
+public class FansParser {
+
+}
