7ba43ddaf6474469110567bfb3c7009374d53a90
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-04 12:16:47 +0000

    将后台运行改为浏览器运行。

--- branches/crawler/weibo-1.1/src/main/java/com/scoop/crawler/weibo/parser/FansParser.java
+++ branches/crawler/weibo-1.1/src/main/java/com/scoop/crawler/weibo/parser/FansParser.java
@@ -1,11 +1,18 @@
 package com.scoop.crawler.weibo.parser;
 
+import com.scoop.crawler.weibo.repository.DataSource;
+import com.scoop.crawler.weibo.request.failed.FailedHandler;
+
 /**
  * 用户粉丝信息解析器。
  * 
  * @author taofucheng
  * 
  */
-public class FansParser {
+public class FansParser extends UserRelationParser {
+
+	public FansParser(DataSource dataSource, FailedHandler handler) {
+		super(dataSource, handler);
+	}
 
 }
