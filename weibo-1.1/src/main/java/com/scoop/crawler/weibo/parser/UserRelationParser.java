fe98ac72c13bd7afc823e2349477f410c8883684
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-04 12:16:47 +0000

    将后台运行改为浏览器运行。

--- weibo-1.1/src/main/java/com/scoop/crawler/weibo/parser/UserRelationParser.java
+++ weibo-1.1/src/main/java/com/scoop/crawler/weibo/parser/UserRelationParser.java
@@ -3,15 +3,9 @@ package com.scoop.crawler.weibo.parser;
 import com.scoop.crawler.weibo.repository.DataSource;
 import com.scoop.crawler.weibo.request.failed.FailedHandler;
 
-/**
- * 用户关注信息解析器
- * 
- * @author taofucheng
- * 
- */
-public class FollowParser extends UserRelationParser {
+public class UserRelationParser extends Parser {
 
-	public FollowParser(DataSource dataSource, FailedHandler handler) {
+	public UserRelationParser(DataSource dataSource, FailedHandler handler) {
 		super(dataSource, handler);
 	}
 
