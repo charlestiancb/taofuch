e51a2181518302ed26e1be0b9c971be0088c7480
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-05 18:15:35 +0000

    增强容错能力！

--- branches/crawler/weibo-1.1/src/main/java/com/scoop/crawler/weibo/runnable/WeiboCommentRunnable.java
+++ branches/crawler/weibo-1.1/src/main/java/com/scoop/crawler/weibo/runnable/WeiboCommentRunnable.java
@@ -1,10 +1,12 @@
 package com.scoop.crawler.weibo.runnable;
 
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.openqa.selenium.WebDriver;
 
 import com.scoop.crawler.weibo.parser.CommentParser;
 import com.scoop.crawler.weibo.repository.DataSource;
 import com.scoop.crawler.weibo.repository.mysql.Weibo;
+import com.scoop.crawler.weibo.request.ExploreRequest;
 import com.scoop.crawler.weibo.request.failed.FailedHandler;
 import com.scoop.crawler.weibo.util.ThreadUtils;
 
@@ -20,8 +22,13 @@ public class WeiboCommentRunnable extends Thread implements Runnable {
 	public void run() {
 		DefaultHttpClient client = ThreadUtils.allocateHttpClient();
 		CommentParser cp = new CommentParser(dataSource, handler);
+		// 整个线程使用一个webDriver，即一个浏览器界面。防止过多的登录被新浪封闭
+		WebDriver driver = ExploreRequest.getDriver("http://weibo.com/");
 		for (Weibo w = dataSource.getOneUnfetchedWeibo(); w != null; w = dataSource.getOneUnfetchedWeibo()) {
-			cp.fetchWeiboComments(w, client);
+			cp.fetchWeiboComments(driver, w, client);
+		}
+		if (driver != null) {
+			driver.quit();
 		}
 		// 将线程释放
 		ThreadUtils.freeThread();
