5de074ce69448a8a4906733b58282b109bdc4d8e
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-20 06:57:48 +0000

    处理对词搜索的功能

--- word-process/src/test/java/com/focustech/abiz/search/spell/CnAndPingyinAPITest.java
+++ word-process/src/test/java/com/focustech/abiz/search/spell/CnAndPingyinAPITest.java
@@ -10,7 +10,7 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.focustech.abiz.search.BaseTest;
-import com.tfc.word.spell.PingyinAPI;
+import com.tfc.word.spell.PinyinAPI;
 
 /**
  * CnAndPingyinAPITest.java
@@ -21,35 +21,39 @@ public class CnAndPingyinAPITest extends BaseTest {
 
 	@Test
 	public void testToPingyin() {
-		Assert.assertTrue(contains("tongzhi", PingyinAPI.toPingyin("同志")));
-		Assert.assertTrue(contains("yueqi", PingyinAPI.toPingyin("乐器")));
-		System.err.println(Arrays.toString(PingyinAPI.toPingyin("明天和我说观后感")));
+		Assert.assertTrue(contains("tongzhi", PinyinAPI.toPinyin("同志")));
+		Assert.assertTrue(contains("yueqi", PinyinAPI.toPinyin("乐器")));
+		System.err.println(Arrays.toString(PinyinAPI.toPinyin("明天和我说观后感")));
 	}
 
 	@Test
 	public void testToFirstLetter() {
-		System.out.println(Arrays.toString(PingyinAPI.toFirstLetter("明天和我说观后感")));
-		System.out.println(Arrays.toString(PingyinAPI.toFirstLetter("一了百了")));
-		System.out.println(Arrays.toString(PingyinAPI.toFirstLetter("乐器")));
-		System.out.println(Arrays.toString(PingyinAPI.toFirstLetter("我们一起一唱百和")));
+		System.out.println(Arrays.toString(PinyinAPI.toFirstLetter("明天和我说观后感")));
+		System.out.println(Arrays.toString(PinyinAPI.toFirstLetter("一了百了")));
+		System.out.println(Arrays.toString(PinyinAPI.toFirstLetter("乐器")));
+		System.out.println(Arrays.toString(PinyinAPI.toFirstLetter("我们一起一唱百和")));
 	}
 
 	@Test
 	public void testToCn() {
-		Assert.assertTrue(contains("通知字", PingyinAPI.toCn("TongZhi字")));
-		Assert.assertTrue(contains("同志字", PingyinAPI.toCn("TongZhi字")));
-		Assert.assertTrue(contains("统治字", PingyinAPI.toCn("TongZhi字")));
-		System.out.println(Arrays.toString(PingyinAPI.toCn("yiLiaoBaiLiao")));
+		Assert.assertTrue(contains("通知字", PinyinAPI.toCn("TongZhi字")));
+		Assert.assertTrue(contains("同志字", PinyinAPI.toCn("TongZhi字")));
+		Assert.assertTrue(contains("统治字", PinyinAPI.toCn("TongZhi字")));
+		System.out.println(Arrays.toString(PinyinAPI.toCn("yiLiaoBaiLiao")));
 	}
 
 	@Test
 	public void testGetSamePinyinWords() {
-		Assert.assertTrue(contains("钢铁ya", PingyinAPI.getSamePinyinWords("港铁ya")));
-		Assert.assertTrue(contains("一唱百和", PingyinAPI.getSamePinyinWords("一chang百和")));
+		Assert.assertTrue(contains("钢铁ya", PinyinAPI.getSamePinyinWords("港铁ya")));
+		Assert.assertTrue(contains("一唱百和", PinyinAPI.getSamePinyinWords("一chang百和")));
 		// Assert.assertTrue(contains("统治字",
 		// CnAndPingyinAPI.getSamePinyinWords("TongZhi字")));
 		// Assert.assertTrue(contains("统治字",
 		// CnAndPingyinAPI.getSamePinyinWords("TongZhi字")));
 		// System.err.println(Arrays.toString(CnAndPingyinAPI.getSamePinyinWords("一chang百和")));
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(PinyinAPI.toPinyinSplit(">起重de设备111")));
 	}
 }
