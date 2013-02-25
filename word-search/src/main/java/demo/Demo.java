b311e0bf3f4588c29995fc89faf05ba1735594cb
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-25 03:55:11 +0000

    完善自动学习新词功能

--- word-search/src/main/java/demo/Demo.java
+++ word-search/src/main/java/demo/Demo.java
@@ -1,4 +1,5 @@
 package demo;
+
 import com.tfc.analysis.KWSeeker;
 import com.tfc.analysis.entity.Keyword;
 import com.tfc.analysis.fragment.HTMLFragment;
@@ -10,12 +11,12 @@ public class Demo {
 		// 添加一个词
 		kw1.addWord(new Keyword("test3"));
 		// 使用默认的高亮方式将文本中含有上面指定的所有词显示出来！
-		kw1.highlight("这是tes1,要注意哦！");
+		System.out.println(kw1.highlight("这是test1,要注意哦！"));
 		// 使用HTML页面加粗的高亮方式将文本中含有上面指定的所有词显示出来！
-		kw1.highlight("这是tes1,要注意哦！", new HTMLFragment("<b>", "</b>"));
+		System.out.println(kw1.highlight("这是test1,要注意哦！", new HTMLFragment("<b>", "</b>")));
 		// 找出文本中所有含有上面词库中的词！
-		kw1.findWords("这是tes1,要注意哦！");
+		System.out.println(kw1.findWords("这是test1,要注意哦！"));
 		// 使用指定的processor（如：WordFinder找出文本中所有含有上面词库中的词）对文本进行处理！
-		kw1.process(new WordFinder(), "这是tes1,要注意哦！", null);
+		System.out.println(kw1.process(new WordFinder(), "这是tes1,要注意哦！", null));
 	}
 }
