b40be3b2d54a8da9f7aa0c9bba7875c10c04e65d
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-26 07:46:14 +0000

    完善自动学习新词功能

--- word-auto-collect/src/main/java/com/tfc/word/auto/collect/config/Configuration.java
+++ word-auto-collect/src/main/java/com/tfc/word/auto/collect/config/Configuration.java
@@ -1,8 +1,13 @@
 package com.tfc.word.auto.collect.config;
 
+import com.tfc.word.auto.collect.repository.JdbcRepository;
+import com.tfc.word.auto.collect.repository.Repository;
+
 public class Configuration {
 	/** 最多可以有几个线程同时在学习！ */
 	public static int THREAD_COUNT = 5;
 	/** 自动审核通过的数字！ */
 	public static int AUTO_CHECKED_NUM = 20;
+	/** 存储仓库 */
+	public static Repository repo = new JdbcRepository();
 }
