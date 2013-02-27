d5af0341d92ebda3e35700750b26ca919e0643e3
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-02-27 03:25:28 +0000

    完善自动学习新词功能

--- trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/service/WordAnalyzerService.java
+++ trunk/word-auto-collect/src/main/java/com/tfc/word/auto/collect/study/service/WordAnalyzerService.java
@@ -57,7 +57,6 @@ public class WordAnalyzerService {
 				}
 			}
 		}
-		System.out.println(words);
 	}
 
 	/**
--- trunk/word-auto-collect/src/main/resources/init/design/tables.sql
+++ trunk/word-auto-collect/src/main/resources/init/design/tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE `word_base` (
   `special_type` varchar(20) DEFAULT '0' COMMENT '专用类型，如学科分类。0：通用类型。',
   `check_status` varchar(20) DEFAULT '0' COMMENT '审核状态，是否已经被审核通过。0：未审核；1：审核通过',
   `stat_num` bigint(20) DEFAULT 0 COMMENT '自动学习时，统计得到的数字',
-  `add_time` DATETIME DEFAULT now() COMMENT '该词加入到表中的时间',
+  `add_time` DATETIME DEFAULT NULL COMMENT '该词加入到表中的时间',
   `checked_time` DATETIME DEFAULT NULL COMMENT '该词审核通过的时间',
   PRIMARY KEY (`rec_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '词语的基本信息';
@@ -17,6 +17,6 @@ CREATE TABLE `word_base` (
 CREATE TABLE `fetch_orig` (
   `rec_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `orig` varchar(5000) NOT NULL COMMENT '具体的来源，如URL或文件路径与名称',
-  `add_time` DATETIME DEFAULT now() COMMENT '该url加入到表中的时间，即抓取解析的时间',
+  `add_time` DATETIME NOT NULL COMMENT '该url加入到表中的时间，即抓取解析的时间',
   PRIMARY KEY (`rec_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '解析词的来源信息，如记录从哪儿获取的信息，防止重复读取！';
