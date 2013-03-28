9c7894f92b97ded887dbc5c92a1c6c685600bcdf
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-03-28 08:10:48 +0000

    修正爬虫的记录功能

--- weibo-1.1/src/main/java/com/scoop/crawler/weibo/entity/LogonInfo.java
+++ weibo-1.1/src/main/java/com/scoop/crawler/weibo/entity/LogonInfo.java
@@ -10,14 +10,12 @@ public class LogonInfo {
 	/** WebDriver每次重新启动一个新的间隔时间 */
 	public static long DRIVER_INTERVAL = 12 * 3600 * 1000L;
 	private static LogonInfo logon;
-	private static long preTime;
 	private String username;
 	private String password;
 
 	private LogonInfo(String username, String password) {
 		setUsername(username);
 		setPassword(password);
-		setPreTime(System.currentTimeMillis());
 	}
 
 	public static void store(String username, String password) {
@@ -49,13 +47,4 @@ public class LogonInfo {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public static void setPreTime(long preTime) {
-		LogonInfo.preTime = preTime;
-	}
-
-	public static long getPreTime() {
-		return preTime;
-	}
-
 }
