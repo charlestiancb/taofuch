9605df78c51ac9f09e293d1bbac93520f9b206f6
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-05-15 05:42:59 +0000

    完善语言模型的代码

---
+++ shensi/language-model/src/main/java/com/ss/language/model/dirichlet/WordInDocumentProcess.java
@@ -0,0 +1,18 @@
+package com.ss.language.model.dirichlet;
+
+import com.ss.language.model.pipe.PipeNode;
+
+/**
+ * 计算P(w|D)的值
+ * 
+ * @author taofucheng
+ * 
+ */
+public class WordInDocumentProcess extends PipeNode {
+
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+
+	}
+}
