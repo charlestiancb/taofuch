46fcd962c868ab23336906bf57eb83315c243c98
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-05-15 05:42:59 +0000

    完善语言模型的代码

---
+++ trunk/shensi/language-model/src/main/java/com/ss/language/model/dirichlet/WordInCollectionProcess.java
@@ -0,0 +1,18 @@
+package com.ss.language.model.dirichlet;
+
+import com.ss.language.model.pipe.PipeNode;
+
+/**
+ * 计算P(w|coll)的值
+ * 
+ * @author taofucheng
+ * 
+ */
+public class WordInCollectionProcess extends PipeNode {
+
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ trunk/shensi/language-model/src/main/java/com/ss/language/model/dirichlet/WordOfLdaProcess.java
@@ -0,0 +1,18 @@
+package com.ss.language.model.dirichlet;
+
+import com.ss.language.model.pipe.PipeNode;
+
+/**
+ * 计算lda模型下P(w|D)的值
+ * 
+ * @author taofucheng
+ * 
+ */
+public class WordOfLdaProcess extends PipeNode {
+
+	@Override
+	public void process() {
+		// TODO Auto-generated method stub
+
+	}
+}
