a3b89ede7aa63620062f654cdb6af83fc121f917
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-05-23 08:56:15 +0000

    完善语言模型的代码

--- shensi/language-model/src/main/java/com/ss/language/model/Main.java
+++ shensi/language-model/src/main/java/com/ss/language/model/Main.java
@@ -1,7 +1,6 @@
 package com.ss.language.model;
 
 import com.ss.language.model.dirichlet.DirichletDistributionProcess;
-import com.ss.language.model.dirichlet.WordInCollectionProcess;
 import com.ss.language.model.dirichlet.WordInDocumentProcess;
 import com.ss.language.model.dirichlet.WordOfLdaProcess;
 import com.ss.language.model.pipe.PipeManager;
@@ -16,7 +15,6 @@ public class Main {
 		// 请注意：以下各个节点的注入顺序就是它们的执行顺序，即计算顺序。
 		PipeManager.regist(new DocumentProcessor());// 这是tf*idf值计算
 		PipeManager.regist(new WordInDocumentProcess());// 计算P(w|D)值
-		PipeManager.regist(new WordInCollectionProcess());// 计算P(w|coll)值
 		PipeManager.regist(new WordOfLdaProcess());// 计算lda模型下的P(w|D)值
 		PipeManager.regist(new DirichletDistributionProcess());// 这是狄利克雷分布计算平滑参数，即将上面三个P值计算结果进行平滑处理。
 
