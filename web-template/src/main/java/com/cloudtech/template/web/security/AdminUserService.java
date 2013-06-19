6c4c853f6ca8f6b1a1c753c09f02457bc6286456
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-06-19 11:22:04 +0000

    完善项目

---
+++ web-template/src/main/java/com/cloudtech/template/web/security/AdminUserService.java
@@ -0,0 +1,20 @@
+package com.cloudtech.template.web.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * 后台权限相关的用户资源操作的服务类
+ * 
+ * @author taofucheng
+ * 
+ */
+public class AdminUserService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
