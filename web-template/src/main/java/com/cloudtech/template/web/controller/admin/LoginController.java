22d0611e02eec5a2f1275197cd2292d437de6b02
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-06-19 11:28:25 +0000

    完善项目

--- web-template/src/main/java/com/cloudtech/template/web/controller/admin/LoginController.java
+++ web-template/src/main/java/com/cloudtech/template/web/controller/admin/LoginController.java
@@ -1,4 +1,4 @@
-package com.cloudtech.template.web.admin.controller;
+package com.cloudtech.template.web.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
