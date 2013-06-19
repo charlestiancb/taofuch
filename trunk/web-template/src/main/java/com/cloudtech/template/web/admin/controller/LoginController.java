ba5e6addf91e3ad4e8064bfd6d10571c944c9ce2
hellohank1 <hellohank1@5889d34c-57f3-950e-ffdf-562491b566ed>
2013-06-19 11:22:04 +0000

    完善项目

---
+++ trunk/web-template/src/main/java/com/cloudtech/template/web/admin/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.cloudtech.template.web.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cloudtech.template.web.controller.BaseController;
+
+/**
+ * 后台管理登录相关的Controller
+ * 
+ * @author taofucheng
+ * 
+ */
+@Controller
+@RequestMapping("/admin")
+public class LoginController extends BaseController {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return null;
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(Model model) {
+		return null;
+	}
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Model model) {
+		return null;
+	}
+}
